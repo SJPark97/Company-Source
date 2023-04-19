80122fd4fee7027893a40d54891a5de102f96628
이강윤(Gangyun Lee) <109258306+GangYunGit@users.noreply.github.com>
2023-04-19 17:57:34 +0900

    feat: 랜딩 페이지 수정 중

--- front/src/pages/index.tsx
+++ front/src/pages/index.tsx
@@ -5,8 +5,11 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main className="flex flex-col text-50">
-      <p>Company Source</p>
-    </main>
+    <div className='flex flex-col'>
+      <div className='text-white text-[4.2vw] w-[60vw] ml-[10%] mt-[6%]'>
+        기업 분석이 어려우신가요?SOURCE와 함께 해보세요.
+      </div>
+      <Image className='fixed -z-10' src="/landing-first.png" alt="landing-first" width={1920} height={1080} />
+    </div>
   )
 }
--- front/src/pages/landing.tsx
+++ front/src/pages/landing.tsx
@@ -1,7 +1,12 @@
+import Image from "next/image"
+import backgroundImage from "/landing-first.png"
+
 export default function landing() {
 	return (
-		<div className="flex flex-col text-100">
-			<p>Landing Page</p>
+		<div style={{ backgroundImage: `url(${backgroundImage})` }}>
+			<div className="flex flex-col">
+				<p>기업 분석이 어려우신가요?SOURCE와 함께 해보세요.</p>
+			</div>
 		</div>
 	)
 }
