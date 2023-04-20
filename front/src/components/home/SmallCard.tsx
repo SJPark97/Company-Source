1f6a9bb3e8196ec253ce8ae249935f809e4c707a
haninan94 <yuyu1172@gmail.com>
2023-04-20 17:56:01 +0900

    feat : Home화면에 있는 BigCard와 SmallCard디자인

--- front/src/components/home/SmallCard.tsx
+++ front/src/components/home/SmallCard.tsx
@@ -12,7 +12,7 @@ export default function SmallCard() {
         alt="samsung_logo.png"
         width={200}
         height={259}
-        className="shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)] rounded-10 p-20"
+        className="shadow-[10px_20px_10px_-15px_rgba(0,0,0,0.3)] rounded-10 p-20"
       />
       <div className="my-auto mx-[10px]">
         삼성전자(SAMSUNG ELECTRONICS)는 대한민국에 본사를 두고 전자제품을 ...
