f8641e63a6743d5eaa56757c4f03cc872778899d
haninan94 <yuyu1172@gmail.com>
2023-04-26 09:29:22 +0900

    feat : chart drawing

---
+++ front/src/components/detail/Chart103.tsx
@@ -0,0 +1,13 @@
+import React, { PureComponent } from 'react';
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+
+
+interface ChartLabel
+
+export default function Chart103() {
+    return (
+        <>
+            <div>ㅇㅇㅇ</div>
+        </>
+    )
+}
