25b30fba73a215cacf5a1a6acc194feb779a12af
kimjinho-dev <109258271+kimjinho-dev@users.noreply.github.com>
2023-04-27 15:42:28 +0900

    feat : add cors config

--- back/SourceCompany/SourceCompany/src/main/java/com/jobtang/sourcecompany/api/user/repo/UserRepo.java
+++
@@ -1,9 +0,0 @@
-package com.jobtang.sourcecompany.api.user.repo;
-
-import com.jobtang.sourcecompany.api.user.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface UserRepo extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
