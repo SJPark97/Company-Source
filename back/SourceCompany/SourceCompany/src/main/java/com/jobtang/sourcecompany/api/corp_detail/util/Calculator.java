5bde5b4e280d1b584757b5c1e5e01a74472ee243
김성준 <joyksj94@gmail.com>
2023-04-27 17:06:28 +0900

    Fix : 오류수정

--- back/SourceCompany/SourceCompany/src/main/java/com/jobtang/sourcecompany/api/corp_detail/util/Calculator.java
+++ back/SourceCompany/SourceCompany/src/main/java/com/jobtang/sourcecompany/api/corp_detail/util/Calculator.java
@@ -7,19 +7,25 @@ public class Calculator {
     // a/b
     public Double myDivision(Number a, Number b) {
         if (a == null || b == null) {return null;}
-        return ((double) a) / ((double) b);
+        Double x = Double.valueOf(a.intValue());
+        Double y = Double.valueOf(b.intValue());
+        return x / y;
     }
 
     // a/b * 100
     public Double myRatio(Number a, Number b) {
         if (a == null || b == null) {return null;}
-        return ((double) a) / ((double) b) * 100;
+        Double x = Double.valueOf(a.intValue());
+        Double y = Double.valueOf(b.intValue());
+        return x / y * 100;
     }
 
     // a + b
     public Double myPlus(Number a, Number b) {
         if (a == null || b == null) {return null;}
-        return (Double) a + (Double) b;
+        Double x = Double.valueOf(a.intValue());
+        Double y = Double.valueOf(b.intValue());
+        return x + y;
     }
 
     // a - b
@@ -31,32 +37,45 @@ public class Calculator {
     // a/(b+c)
     public Double myRatioWithSum(Number a, Number b, Number c) {
         if (a == null || b == null || c == null) {return null;}
-        return (double) a / ((double) b + (double) c) * 100;
+        Double x = Double.valueOf(a.intValue());
+        Double y = Double.valueOf(b.intValue());
+        Double z = Double.valueOf(c.intValue());
+        return x / (y + z) * 100;
     }
 
     // (a-b)/c
     public Double myRatioWithSubtraction(Number a, Number b, Number c) {
         if (a == null || b == null || c == null) {return null;}
-        return ((double) a - (double) b) / (double) c * 100;
+        Double x = Double.valueOf(a.intValue());
+        Double y = Double.valueOf(b.intValue());
+        Double z = Double.valueOf(c.intValue());
+        return (x - y) / z * 100;
     }
 
     // a * b
     public Double myMultiply(Number a, Number b) {
         if (a == null || b == null) {return null;}
-        return (double) a * (double) b;
+        Double x = Double.valueOf(a.intValue());
+        Double y = Double.valueOf(b.intValue());
+        return x * y;
     }
 
     // a*(b+c)
     public Double myMultiplyWithSum(Number a, Number b, Number c) {
         if (a == null || b == null || c == null) {return null;}
-        return (double) a * ((double) b + (double) c);
+        Double x = Double.valueOf(a.intValue());
+        Double y = Double.valueOf(b.intValue());
+        Double z = Double.valueOf(c.intValue());
+        return x * (y + z);
 
     }
     // (a-b)/c * d
     public Double myCalculate(Number a, Number b, Number c, Number d) {
         if (a == null || b == null || c == null || d == null) { return null;}
-        return ((double) a - (double) b) / (double) c * (double) d;
+        Double x = Double.valueOf(a.intValue());
+        Double y = Double.valueOf(b.intValue());
+        Double z = Double.valueOf(c.intValue());
+        Double w = Double.valueOf(d.intValue());
+        return (x - y) / z * w;
     }
-
-
 }
