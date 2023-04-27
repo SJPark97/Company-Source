e7e338939a06739ea576f621235054a596d15584
김진호 <bundo10@naver.com>
2023-04-27 15:48:31 +0900

    feat : add cors config and redis rand corp setting api

--- back/SourceCompany/SourceCompany/src/main/java/com/jobtang/sourcecompany/api/user/repo/UserRepository.java
+++ back/SourceCompany/SourceCompany/src/main/java/com/jobtang/sourcecompany/api/user/repo/UserRepository.java
@@ -4,6 +4,6 @@ import com.jobtang.sourcecompany.api.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
-public interface UserRepo extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
     User findByUsername(String username);
 }
