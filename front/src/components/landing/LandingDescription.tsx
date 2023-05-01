e18f7e8ba7115bb007c3bcf804a43e42753a7473
haninan94 <yuyu1172@gmail.com>
2023-05-01 11:26:30 +0900

    feat : 랜딩 페이지 애니메이션 수정

--- front/src/components/landing/LandingDescription.tsx
+++ front/src/components/landing/LandingDescription.tsx
@@ -1,4 +1,5 @@
-import { useRef, useLayoutEffect, useState } from "react";
+import { useRef } from "react";
+import { motion, useScroll } from "framer-motion";
 
 interface Iprops {
   title: string;
@@ -6,46 +7,36 @@ interface Iprops {
 }
 
 export default function LandingDiscription({ title, content }: Iprops) {
-  const [isInViewport1, setIsInViewport1] = useState(false);
-  const [isInViewport2, setIsInViewport2] = useState(false);
-
-  const ref1 = useRef<HTMLDivElement>(null);
-  const ref2 = useRef<HTMLDivElement>(null);
-
-  useLayoutEffect(() => {
-    const handleScroll = () => {
-      if (ref1.current) {
-        const { top, bottom } = ref1.current.getBoundingClientRect();
-        setIsInViewport1(top < window.innerHeight && bottom > 0);
-      }
-      if (ref2.current) {
-        const { top, bottom } = ref2.current.getBoundingClientRect();
-        setIsInViewport2(top < window.innerHeight && bottom > 0);
-      }
-    };
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
   return (
     <div className="flex flex-col items-center">
-      <div
-        className={
-          "text-36 font-bold w-[32vw] ml-[10vw] whitespace-pre-line tracking-tighter " +
-          `${isInViewport1 ? "animate-fadeIn" : ""}`
-        }
-        ref={ref1}
+      <motion.div
+        initial={{ opacity: 0, y: 50 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 3 }}
+        viewport={{ once: true }}
       >
-        {title}
-      </div>
-      <div
-        className={
-          "text-28 text-[#aaaaaa] font-bold w-[30vw] mt-[1vh] ml-[10vw] whitespace-pre-line tracking-tighter " +
-          `${isInViewport2 ? "animate-fadeIn" : ""}`
-        }
-        ref={ref2}
+        <div
+          className={
+            "text-36 font-bold w-[32vw] ml-[10vw] whitespace-pre-line tracking-tighter"
+          }
+        >
+          {title}
+        </div>
+      </motion.div>
+      <motion.div
+        initial={{ opacity: 0, y: 100 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 2 }}
+        viewport={{ once: true }}
       >
-        {content}
-      </div>
+        <div
+          className={
+            "text-28 text-[#aaaaaa] font-bold w-[30vw] mt-[1vh] ml-[10vw] whitespace-pre-line tracking-tighter"
+          }
+        >
+          {content}
+        </div>
+      </motion.div>
     </div>
   );
 }
