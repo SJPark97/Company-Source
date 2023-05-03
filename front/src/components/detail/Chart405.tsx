890b15bd9041b0f571d9d572403493d399b8f0fb
이강윤(Gangyun Lee) <109258306+GangYunGit@users.noreply.github.com>
2023-05-03 16:09:25 +0900

    feat: 툴팁 추가 및 차트 수정 중

--- front/src/components/detail/Chart405.tsx
+++ front/src/components/detail/Chart405.tsx
@@ -1,5 +1,63 @@
-export default function Chart405() {
+import React, { useEffect, useState } from "react";
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+} from "recharts";
+import AnalysisResult from "./AnalysisResult";
+import Legend from "./Legend";
+
+interface Iprops {
+  chartData: any;
+}
+
+export default function Chart405({ chartData }: Iprops) {
+  const [data, setData] = useState<any>();
+
+  useEffect(() => {
+    setData(chartData);
+  }, []);
+
   return (
-    <div></div>
-  )
+    <>
+      <div className="flex justify-center flex-nowrap text-12">
+        {data
+          ? data.data.result.map((item: any) => {
+            console.log(item);
+            return (
+              <>
+                <BarChart
+                  width={120}
+                  height={300}
+                  data={[item]}
+                  margin={{
+                    top: 40,
+                    right: 30,
+                    left: 0,
+                    bottom: 40,
+                  }}
+                >
+                  <CartesianGrid strokeDasharray="3 3" />
+                  <XAxis dataKey="name" tickMargin={16} interval={1} />
+                  <YAxis />
+                  <Tooltip wrapperStyle={{ zIndex: "50" }} />
+                  {/* <Legend /> */}
+                  <Bar
+                    dataKey={data.data.corp_name}
+                    fill={
+                      item["평가"] === "양호" ? "#82ca9d" : (item["평가"] === "불량" ? "red" : "#fef33f")
+                    }
+                  />
+                  <Bar dataKey="산업평균" fill="#8884d8" />
+                </BarChart>
+              </>
+            );
+          })
+          : "데이터가 없어요 ㅠㅠ"}
+      </div>
+    </>
+  );
 }
--- front/src/components/detail/Title.tsx
+++ front/src/components/detail/Title.tsx
@@ -12,7 +12,7 @@ export default function Title({ name, companyLogo }: Iprops) {
 			<div className="flex font-bold text-24">
 				{companyLogo ?
 					<Image src={companyLogo} alt="companyLogo" width={82} height={30} className="mr-16" />
-					: null}
+					: <Image src="/logo.png" alt="companyLogo" width={82} height={30} className="my-20 mr-16" />}
 				<span className="self-center">{name}</span>
 			</div>
 		</>
