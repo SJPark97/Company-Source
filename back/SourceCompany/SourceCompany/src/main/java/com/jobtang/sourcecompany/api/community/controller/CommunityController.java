d5479dce62688f24677dbc44d19a243d82062c63
hyunho <potato3884@naver.com>
2023-05-03 10:24:49 +0900

    Feat : community 글작성 1차 구현

--- back/SourceCompany/SourceCompany/src/main/java/com/jobtang/sourcecompany/api/community/controller/CommunityController.java
+++ back/SourceCompany/SourceCompany/src/main/java/com/jobtang/sourcecompany/api/community/controller/CommunityController.java
@@ -1,12 +1,18 @@
 package com.jobtang.sourcecompany.api.community.controller;
 
 
+import com.jobtang.sourcecompany.api.community.dto.CreateCommunityRequest;
 import com.jobtang.sourcecompany.api.community.service.CommunityService;
+import com.jobtang.sourcecompany.api.corp.dto.CorpSearchListDto;
+import com.jobtang.sourcecompany.api.user.entity.User;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -16,5 +22,27 @@ import org.springframework.web.bind.annotation.RestController;
 public class CommunityController {
   private  final CommunityService communityService;
 
+  @ApiOperation(
+          value = "작성한 기업관련 글을 DB에 넣어주는 API",
+          notes = "현재 로그인한 유저 명의로 기업분석 게시글 작성",
+          response = CorpSearchListDto.class,
+          responseContainer = "List"
+  )
+  @PostMapping
+  public ResponseEntity<?> randCorp(@RequestBody CreateCommunityRequest createCommunityRequest) {
+    /*
+    jwt token을 통해 User 객체 가져오는 코드로 대체
+    User user = token.getLoginedUser();
+     */
+    User user = null;
+    HttpHeaders headers = new HttpHeaders();
+    try{
+      communityService.createCommunity(user , createCommunityRequest);
+    }
+    catch (Exception e){
+      e.printStackTrace();
+    }
 
+    return new ResponseEntity<>( headers, HttpStatus.CREATED);
+  }
 }
--- back/SourceCompany/SourceCompany/src/main/java/com/jobtang/sourcecompany/api/community/service/CommunityService.java
+++ back/SourceCompany/SourceCompany/src/main/java/com/jobtang/sourcecompany/api/community/service/CommunityService.java
@@ -1,4 +1,8 @@
 package com.jobtang.sourcecompany.api.community.service;
 
+import com.jobtang.sourcecompany.api.community.dto.CreateCommunityRequest;
+import com.jobtang.sourcecompany.api.user.entity.User;
+
 public interface CommunityService {
+  void createCommunity(User user, CreateCommunityRequest createComunityRequest);
 }
