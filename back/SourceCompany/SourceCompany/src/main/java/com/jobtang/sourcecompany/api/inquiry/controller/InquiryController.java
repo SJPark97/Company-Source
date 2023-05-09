3ebd6670d960362e44848b443f7bf49c30922a94
SJPark97 <tjwlsdud33@naver.com>
2023-05-09 16:37:23 +0900

    Feat: 문의게시판 post완료

--- back/SourceCompany/SourceCompany/src/main/java/com/jobtang/sourcecompany/api/inquiry/controller/InquiryController.java
+++ back/SourceCompany/SourceCompany/src/main/java/com/jobtang/sourcecompany/api/inquiry/controller/InquiryController.java
@@ -3,6 +3,8 @@ package com.jobtang.sourcecompany.api.inquiry.controller;
 
 import com.jobtang.sourcecompany.api.inquiry.dto.CreateInquiryRequest;
 import com.jobtang.sourcecompany.api.inquiry.service.InquiryService;
+import com.jobtang.sourcecompany.api.user.service.JwtService;
+import com.jobtang.sourcecompany.config.JwtTokenProvider;
 import io.swagger.annotations.Api;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -20,11 +22,13 @@ import java.util.HashMap;
 public class InquiryController {
 
     private final InquiryService inquiryService;
+    private final JwtService jwtService;
+
     @PostMapping("")
     public ResponseEntity<?> createInquiry(@RequestBody CreateInquiryRequest createInquiryRequest,
                                             @RequestHeader("Authorization") String authHeader) {
         HashMap<String, Object> result = new HashMap<>();
-        Long userId = Long.parseLong(authHeader.substring(7));
+        Long userId = jwtService.userPkByToken(authHeader);
         inquiryService.createInquiry(userId, createInquiryRequest);
         result.put("data", "success");
         return new ResponseEntity<>(result, HttpStatus.CREATED);
--- back/SourceCompany/SourceCompany/src/main/java/com/jobtang/sourcecompany/api/inquiry/repository/InquiryRepository.java
+++ back/SourceCompany/SourceCompany/src/main/java/com/jobtang/sourcecompany/api/inquiry/repository/InquiryRepository.java
@@ -4,6 +4,6 @@ import com.jobtang.sourcecompany.api.inquiry.entity.Inquiry;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
+
 public interface InquiryRepository extends JpaRepository<Inquiry, Long> {
 }
