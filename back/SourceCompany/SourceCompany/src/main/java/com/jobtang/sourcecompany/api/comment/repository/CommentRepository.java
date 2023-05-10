2389ca9fb125c3cbfc6c3e7d8988ac2e2795fd65
최현호 <potato3884@naver.com>
2023-05-10 15:38:14 +0900

    Feat : comment 작성중

--- back/SourceCompany/SourceCompany/src/main/java/com/jobtang/sourcecompany/api/comment/repository/CommentRepository.java
+++ back/SourceCompany/SourceCompany/src/main/java/com/jobtang/sourcecompany/api/comment/repository/CommentRepository.java
@@ -3,5 +3,8 @@ package com.jobtang.sourcecompany.api.comment.repository;
 import com.jobtang.sourcecompany.api.comment.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CommentRepository extends JpaRepository<Comment , Long> {
+  List<Comment> findAllByIdAndIsActiveTrueOrderByCommentGroupAscCreatedDateAsc(Long commentId);
 }
