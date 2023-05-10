75d36fe67b41bcfc85938b7547beb2caf2c011ef
김성준 <joyksj94@naver.com>
2023-05-10 15:36:34 +0900

    Fix : 결과 엔티티 추가

---
+++ back/SourceCompany/SourceCompany/src/main/java/com/jobtang/sourcecompany/api/analysis_result/service/AnalysisResultService.java
@@ -0,0 +1,8 @@
+package com.jobtang.sourcecompany.api.analysis_result.service;
+
+import com.jobtang.sourcecompany.api.corp.entity.Corp;
+
+public interface AnalysisResultService {
+    // 분석 결과 저장
+    void updateAnalysisResult(Corp corp, String analysisId, String rate);
+}
