f894aae944ec3b1564d186054c5bd86b97f2a750
hyunho <potato3884@naver.com>
2023-05-12 14:14:12 +0900

    wow

--- back/SourceCompany/SourceCompany/src/main/java/com/jobtang/sourcecompany/api/comment/repository/CommentRepository.java
+++ back/SourceCompany/SourceCompany/src/main/java/com/jobtang/sourcecompany/api/comment/repository/CommentRepository.java
@@ -3,8 +3,17 @@ package com.jobtang.sourcecompany.api.comment.repository;
 import com.jobtang.sourcecompany.api.comment.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import javax.swing.text.html.Option;
 import java.util.List;
+import java.util.Optional;
 
 public interface CommentRepository extends JpaRepository<Comment , Long> {
-  List<Comment> findAllByIdAndIsActiveTrueOrderByCommentGroupAscCreatedDateAsc(Long commentId);
+  List<Comment> findAllByCommunityIdAndIsActiveTrueOrderByCommentGroupAscCreatedDateAsc(Long communityId);
+
+  List<Comment> findByCommentGroup(Long commentGroup);
+
+  Optional<Comment>  findByCommentGroupAndParent1FalseAndIsActiveFalse (Long commentGroup);
+
+  // 모든 자식 댓글들 구하는 메소드
+  List<Comment> findByCommentGroupAndIsActiveTrueAndParent0 (Long commentGroup);
 }
