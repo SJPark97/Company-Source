6351f447ae283866d0d69c938c2dcedc9b717d23
최현호 <potato3884@naver.com>
2023-05-12 15:38:10 +0900

    Feat : community 상세조회시 댓글 조회 기능 구현

--- back/SourceCompany/SourceCompany/src/main/java/com/jobtang/sourcecompany/api/comment/dto/CreateCommentRequest.java
+++ back/SourceCompany/SourceCompany/src/main/java/com/jobtang/sourcecompany/api/comment/dto/CreateCommentRequest.java
@@ -8,10 +8,13 @@ import lombok.NoArgsConstructor;
 public class CreateCommentRequest {
   private String content;
 
+  // 달린 communityId를 따라간다.
   private Long communityId;
 
+  // commentId 값을 따라간다.
   private Long commentGroup;
 
+  // 부모는 1 자식은 0
   private Long parent;
 
 }
