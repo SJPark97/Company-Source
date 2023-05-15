a6b892efbd6d989108a52e88fccc87db2d24653b
최현호 <potato3884@naver.com>
2023-05-15 11:12:20 +0900

    Fix: community image table에 user column

--- back/SourceCompany/SourceCompany/src/main/java/com/jobtang/sourcecompany/api/community_image/entity/CommunityImage.java
+++ back/SourceCompany/SourceCompany/src/main/java/com/jobtang/sourcecompany/api/community_image/entity/CommunityImage.java
@@ -1,8 +1,10 @@
 package com.jobtang.sourcecompany.api.community_image.entity;
 
+import com.jobtang.sourcecompany.api.user.entity.User;
 import com.jobtang.sourcecompany.util.BaseEntity;
 import com.sun.istack.NotNull;
 import lombok.*;
+import net.minidev.json.annotate.JsonIgnore;
 
 import javax.persistence.*;
 
@@ -21,4 +23,9 @@ public class CommunityImage extends BaseEntity {
     @NotNull
     @Column(nullable = false)
     private String communityImageUrl;
+
+    @JsonIgnore
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
 }
--- back/SourceCompany/SourceCompany/src/main/java/com/jobtang/sourcecompany/api/community_image/service/CommunityImageServiceImpl.java
+++ back/SourceCompany/SourceCompany/src/main/java/com/jobtang/sourcecompany/api/community_image/service/CommunityImageServiceImpl.java
@@ -3,11 +3,16 @@ package com.jobtang.sourcecompany.api.community_image.service;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.jobtang.sourcecompany.api.community_image.entity.CommunityImage;
+import com.jobtang.sourcecompany.api.community_image.repository.CommunityImageRepository;
 import com.jobtang.sourcecompany.api.exception.CustomException;
 import com.jobtang.sourcecompany.api.exception.ErrorCode;
+import com.jobtang.sourcecompany.api.user.entity.User;
+import com.jobtang.sourcecompany.api.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -19,11 +24,16 @@ import java.util.HashMap;
 @RequiredArgsConstructor
 public class CommunityImageServiceImpl implements CommunityImageService {
     private final AmazonS3Client amazonS3Client;
+    private final CommunityImageRepository communityImageRepository;
     @Value("${cloud.aws.s3.bucket}")
     private String bucket;
+    private final UserRepository userRepository;
+
     @Override
+    @Transactional
     public HashMap<String, Object> createCommunityImage(MultipartFile file, Long userId) {
         HashMap<String, Object> result = new HashMap<>();
+        User user = userRepository.findById(userId).orElseThrow(() -> new CustomException(ErrorCode.USER_EXISTS));
         LocalDateTime now = LocalDateTime.now();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMddHHmmss");
         String fileName = now.format(formatter) + file.getOriginalFilename();
@@ -36,6 +46,11 @@ public class CommunityImageServiceImpl implements CommunityImageService {
             amazonS3Client.putObject(bucket, fileName, file.getInputStream(), metadata);
             amazonS3Client.setObjectAcl(bucket, fileName, CannedAccessControlList.PublicRead);
             result.put("data", fileUrl);
+            CommunityImage communityImage = CommunityImage.builder()
+                    .communityImageUrl(fileUrl)
+                    .user(user)
+                    .build();
+            communityImageRepository.save(communityImage);
         } catch (IOException e) {
             // 에러 처리
             throw new CustomException(ErrorCode.AWS_ERROR);
