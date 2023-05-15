fe82a63d4f9eee5dc44909222296a3799cbc2008
hyunho <potato3884@naver.com>
2023-05-15 11:10:00 +0900

    Fix: community에 user 컬럼 추가

--- back/SourceCompany/SourceCompany/src/main/java/com/jobtang/sourcecompany/api/community_image/repository/CommunityImageRepository.java
+++ back/SourceCompany/SourceCompany/src/main/java/com/jobtang/sourcecompany/api/community_image/repository/CommunityImageRepository.java
@@ -1,4 +1,7 @@
 package com.jobtang.sourcecompany.api.community_image.repository;
 
-public interface CommunityImageRepository {
+import com.jobtang.sourcecompany.api.community_image.entity.CommunityImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CommunityImageRepository extends JpaRepository<CommunityImage, Long> {
 }
