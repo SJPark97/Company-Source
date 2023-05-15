a65a6dcd1f57b3dc6c839587ddefe02bedb4de7c
김성준 <joyksj94@naver.com>
2023-05-15 10:08:41 +0900

    Feat : 매출액 기업 조회 추가

--- back/SourceCompany/SourceCompany/src/main/java/com/jobtang/sourcecompany/api/analysis_result/service/AnalysisResultService.java
+++ back/SourceCompany/SourceCompany/src/main/java/com/jobtang/sourcecompany/api/analysis_result/service/AnalysisResultService.java
@@ -1,16 +1,12 @@
 package com.jobtang.sourcecompany.api.analysis_result.service;
 
-import com.jobtang.sourcecompany.api.analysis_result.Dto.GoodCorpResponseDto;
+import com.jobtang.sourcecompany.api.corp.dto.CorpListResponseDto;
 import com.jobtang.sourcecompany.api.corp.entity.Corp;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-
-import java.util.HashMap;
 
 public interface AnalysisResultService {
     // 분석 결과 저장
     void updateAnalysisResult(Corp corp, String analysisId, String rate);
-    GoodCorpResponseDto GetGoodCorps(int size, int page);
+    CorpListResponseDto GetGoodCorps(int size, int page);
 
 
 }
