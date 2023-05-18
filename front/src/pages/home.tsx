99a6686abb5e389663986ecc1d7890e053395bd9
haninan94 <yuyu1172@gmail.com>
2023-05-18 22:45:05 +0900

    Fix : Home 문구 변경 1개의 분석방법 -> 15개의 분석방법으로 변경

--- front/src/pages/home.tsx
+++ front/src/pages/home.tsx
@@ -163,7 +163,7 @@ export default function Home() {
                 className="font-bold text-white lg:text-26 xl:text-29 2xl:text-32 text-shadow animate-fadeIn"
                 style={{ textShadow: "2px 2px 2px rgba(0, 0, 0, 1)" }}
               >
-                약 1개의 분석 방법으로 분석했습니다.
+                약 15개의 분석 방법으로 분석했습니다.
               </div>
             </div>
             <SearchBar getData={getData} />
